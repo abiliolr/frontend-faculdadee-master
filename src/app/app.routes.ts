08df5ff035e99e3a0162351a5c9d8bb5877ba09e
abiliolr <48859628+abiliolr@users.noreply.github.com>
2025-11-06 11:34:40 -0300

    telas aluno, professor, admin

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,21 +4,22 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { HomeComponent } from './components/home/home.component';
 import { RegisterComponent } from './components/register/register'; // <-- ADICIONAR ISSO
-
+import { AlunoComponent } from './components/aluno/aluno.component';
 export const routes: Routes = [
+  // MUDANÇA 2: Adiciona a rota para /login
+  { path: 'login', component: LoginComponent },
 
-    // MUDANÇA 2: Adiciona a rota para /login
-    { path: 'login', component: LoginComponent },
+  // <-- ADICIONAR ISSO
+  { path: 'register', component: RegisterComponent },
 
-    // <-- ADICIONAR ISSO
-    { path: 'register', component: RegisterComponent }, 
+  { path: 'aluno', component: AlunoComponent },
 
-    // MUDANÇA 3: Adiciona a rota para /home
-    { path: 'home', component: HomeComponent }, // O usuário é enviado para cá após o login
+  // MUDANÇA 3: Adiciona a rota para /home
+  { path: 'home', component: HomeComponent }, // O usuário é enviado para cá após o login
 
-    // MUDANÇA 4: Redireciona a rota raiz (/) para a página de login
-    { path: '', redirectTo: 'login', pathMatch: 'full' },
+  // MUDANÇA 4: Redireciona a rota raiz (/) para a página de login
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
 
-    // MUDANÇA 5: Uma rota "catch-all" (opcional, mas boa prática)
-    { path: '**', redirectTo: 'login' } 
+  // MUDANÇA 5: Uma rota "catch-all" (opcional, mas boa prática)
+  { path: '**', redirectTo: 'login' },
 ];
